71d1dc8d0649dad69354be33dfb425c0ec474766
leoli <jafee201153@gmail.com>
2024-02-28 00:01:51 +0800

    feat: add xFolderBrowserDialog project to solution

---
+++ xFolderBrowserDialog/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace xFolderBrowserDialog;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var folder = new System.xFolderBrowserDialog()
+        {
+            Title = "Select a folder",
+            Multiselect = true
+        };
+
+        if (folder.ShowDialog())
+        {
+            foreach (var folderName in folder.FolderNames)
+            {
+                Console.WriteLine(folderName);
+            }
+        }
+    }
+}
