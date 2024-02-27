894e841bdda3ed051c76c45715f440218f7d0a29
leoli <jafee201153@gmail.com>
2024-02-27 13:51:49 +0800

    feat: add LogHelper project and solution files

---
+++ LogHelper/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LogHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DEBUG_Print("Hello, World!");
+        }
+
+        private static string _sourceName = ".Net Runtime";
+
+        public static void DEBUG_Print(string message, System.Diagnostics.EventLogEntryType type = System.Diagnostics.EventLogEntryType.Information)
+        {
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                System.Diagnostics.EventLog.WriteEntry(_sourceName, message, type);
+            }
+            else
+            {
+                throw new NotSupportedException("Not supported on this platform");
+            }
+        }
+    }
+}
